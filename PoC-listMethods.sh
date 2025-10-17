84577588eb0a49be50632db22420c58cb02bb997
Ruby <northstar6009@gmail.com>
2025-10-17 08:52:14 +0200

    Create PoC-listMethods.sh

---
+++ PoC-listMethods.sh
@@ -0,0 +1,1 @@
+curl -X POST https://www.example.com/xmlrpc.php -d '<?xml version="1.0"?><methodCall><methodName>system.listMethods</methodName><params></params></methodCall>' -H "Content-Type: text/xml"
